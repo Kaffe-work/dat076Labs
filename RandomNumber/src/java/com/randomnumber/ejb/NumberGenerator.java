b9d83ca77a78933e6c3d6cc69400a81d0d9bfb0a
bergrahm <bergrahm@student.chalmers.se>
2019-09-05 13:52:23 +0200

    finish lab4.2

--- RandomNumber/src/java/com/randomnumber/ejb/NumberGenerator.java
+++ RandomNumber/src/java/com/randomnumber/ejb/NumberGenerator.java
@@ -8,6 +8,7 @@ package com.randomnumber.ejb;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
 
 /**
@@ -18,6 +19,8 @@ import javax.faces.view.ViewScoped;
 @ViewScoped
 public class NumberGenerator implements Serializable {
 
+    private double min =0.0;
+    private double max = 1.0;
     private double number = Math.random();
 
     /**
@@ -25,13 +28,30 @@ public class NumberGenerator implements Serializable {
      */
     public NumberGenerator() {
     }
+    
+    public void setMin(String x){
+        this.min = Double.parseDouble(x);
+    }
+    
+    public String getMin(){
+        String mini = ""+ min;
+        return mini;
+    }
+    public String getMax(){
+        String maxi = ""+ max;
+        return maxi;
+    }
+    
+    public void setMax(String x){
+        this.max = Double.parseDouble(x);
+    }
 
     public double getNumber() {
         return (number);
     }
 
     public String randomize() {
-        number = Math.random();
+        number = (Math.random() * (this.max - this.min)) + this.min;
         return (null);
     }
 }
